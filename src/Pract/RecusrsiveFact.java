d570403045d95ef75a2aa8ce47e83ea788496c2c
BalajiDandeShree <balajidande51@gmail.com>
2024-05-27 13:22:10 -0700

    Shree program added

--- src/Pract/RecusrsiveFact.java
+++ src/Pract/RecusrsiveFact.java
@@ -1,10 +1,24 @@
 package Pract;
 
 public class RecusrsiveFact {
-
+public static int  f1=0,f2=1,f3;
 	public static void main(String[] args) {
 		
-
+System.out.print(f1 +" "+ f2);
+factorial(10);
+	}
+	
+	
+	public static void factorial(int a) {
+		if(a<0) {
+			return;
+		}
+		
+		f3 = f1 +f2;
+		System.out.println(f3);
+		f1=f2;
+		f2=f3;
+		factorial(--a);
 	}
 	
 	
---
+++ src/Pract/ShreeTest.java
@@ -0,0 +1,10 @@
+package Pract;
+
+public class ShreeTest {
+
+	public static void main(String[] args) {
+		System.out.println("Hi I am shree");
+
+	}
+
+}
